50bf49706d2cc76ce69adda93d5ad73c19b4f2f0
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-30 14:42:09 +0700

    feat: Section Divider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
+import SectionDivider from '@/components/SectionDivider';
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center px-4'>
       <Header />
+      <SectionDivider />
       <main className=''></main>
     </div>
   );
---
+++ components/SectionDivider.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function SectionDivider() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+      className='bg-gray-200 my-24 w-24 h-1 rounded-full hidden sm:block'
+    ></motion.div>
+  );
+}
