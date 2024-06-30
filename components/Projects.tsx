b043f8a6bd253a1d419ce875498b99ba8edc885a
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-30 19:03:52 +0700

    feat: project section framer motion update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import { projectsData } from '@/app/_lib/data';
 import SectionHeading from './SectionHeading';
 import React, { useRef } from 'react';
 import Image from 'next/image';
-import { motion, useScroll } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Projects() {
   return (
@@ -24,18 +24,26 @@ export default function Projects() {
 type ProjectCardProps = (typeof projectsData)[number];
 
 function ProjectCard({ title, description, tags, imageUrl }: ProjectCardProps) {
-  const ref = useRef<HTMLElement>(null);
-  useScroll({
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['0 1', '1.33 1'],
   });
 
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
-    <motion.div className='group mb-3 sm:mb-8 last:mb-0'>
-      <article
-        ref={ref}
-        className='bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'
-      >
+    <motion.div
+      style={{
+        scale: scaleProgress,
+        opacity: opacityProgress,
+      }}
+      ref={ref}
+      className='group mb-3 sm:mb-8 last:mb-0'
+    >
+      <article className='bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'>{title}</h3>
           <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
